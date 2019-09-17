0d93344afb1bcdfd8d249f7b671a03eb46fe0c69
kotbaton <zzzadey@gmail.com>
2019-09-17 12:13:09 +0200

    Add function for color picking

---
+++ color.py
@@ -0,0 +1,50 @@
+class Attribute:
+    reset       = 0 
+    normal      = 0 
+    bold        = 1 
+    dim         = 2 
+    italic      = 3 
+    underscore  = 4 
+    negative    = 7 
+    strikeout   = 9 
+
+
+def reset():
+    """Reset all aplyed color setting."""
+    return "\033[0m"
+
+
+def by_index(fg_color_index, bg_color_index = 0, attribute = 0):
+    """
+    Return string with ANSI escape code for set text colors 
+
+    fg_color_index: color index from 0 to 255, applied to text
+    bg_color_index: color index from 0 to 255, applied to background
+    attribute: use Attribute class variables
+    """
+    return f"\033[{attribute};38;5;{fg_color_index};48;5;{bg_color_index}m"
+
+def by_hex(fg, bg = "#000000", attribute = 0):
+    """
+    Return string with ANSI escape code for set text colors 
+
+    fg: html hex code for text color
+    bg: html hex code for background color
+    attribute: use Attribute class variables
+    """
+    # Delete #
+    fg = fg.replace("#", "")
+    bg = bg.replace("#", "")
+
+    fg_r = int(fg[0:2], 16)
+    fg_g = int(fg[2:4], 16)
+    fg_b = int(fg[4:6], 16)
+    fg_rgb = f"{fg_r};{fg_g};{fg_b}"
+
+    bg_r = int(bg[0:2], 16)
+    bg_g = int(bg[2:4], 16)
+    bg_b = int(bg[4:6], 16)
+    bg_rgb = f"{bg_r};{bg_g};{bg_b}"
+
+    return f"\033[{attribute};38;2;{fg_rgb};48;2;{bg_rgb}m"
+
--- letters.py
+++ letters.py
@@ -1,18 +1,7 @@
 #!/usr/bin/python3
 import sys
 import fonts
-
-colors = [
-    "\033[0m",
-    "\033[31m",
-    "\033[32m",
-    "\033[33m",
-    "\033[34m",
-    "\033[35m",
-    "\033[36m",
-    "\033[37m",
-    "\033[39m",
-]
+import color
 
 
 def print_rainbowed(text, font, spacing = 2):
@@ -20,9 +9,11 @@ def print_rainbowed(text, font, spacing = 2):
         c = 1
         for l in text:
             if ' ' <= l <= '_':
-                print(f"{' '*(spacing//2)}",
-                      f"{colors[c]}{letter_part[ord(l)-ord(' ')]}{colors[0]}",
-                      f"{' '*(spacing//2)}",
+                print(" "*(spacing//2),
+                      color.by_index(c),
+                      letter_part[ord(l)-ord(' ')],
+                      color.reset(),
+                      " "*(spacing//2),
                       end="", sep="")
                 c = c+1 if c<7 else 1
             else:
@@ -35,30 +26,29 @@ def print_attributed(text,
                      spacing = 2,
                      fg_color = 9,
                      bg_color = 0):
-    print(f"\033[3{fg_color};4{bg_color}m", end="")
+    print(color.by_index(fg_color, bg_color), end="")
     for letter_part in font:
         for l in text:
             if ' ' <= l <= '_':
-                print(f"{' '*(spacing//2)}",
-                      f"{letter_part[ord(l)-ord(' ')]}",
-                      f"{' '*(spacing//2)}",
+                print(" "*(spacing//2),
+                      letter_part[ord(l)-ord(' ')],
+                      " "*(spacing//2),
                       end="", sep="")
             else:
                 print(" "*(spacing*2), end="")
         print()
-    print("\033[0m", end="")
+    print(color.reset(), end="")
 
 
 def main():
+    text = "some text".upper()
     if len(sys.argv) > 1:
         text = " ".join(sys.argv[1:]).upper()
-    else:
-        text = "some text"
 
     print()
     print_rainbowed(text,
                     fonts.font1,
-                    spacing = 1)
+                    spacing = 2)
     #  print_attributed(text,
     #                   fonts.font1,
     #                   spacing = 2,
